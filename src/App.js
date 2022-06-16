1e0346457806d2f3c52ca16e95cf14b7b69e8e4e
Youssef <youssefframy>
2022-06-16 20:09:19 +0200

    adding redux, implemented userReducer and userActions to header and app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom'; 
+import { Switch, Route } from 'react-router-dom'; 
+import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { setCurrentUser } from './redux/user/user.action';
 
 function App() {
   return (
@@ -13,10 +16,15 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
+      <Route exact path='/signIn' component={SignInAndSignUpPage} />
     </Switch>
     </div>
   );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
   
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss'
@@ -16,4 +17,8 @@ const Header = () => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
